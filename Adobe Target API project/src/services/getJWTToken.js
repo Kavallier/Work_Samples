2292b3c7eea30b52d981f96bb7c36f65880d6d4b
Kavallier <30388027+Kavallier@users.noreply.github.com>
2021-08-23 09:13:58 +1000

    Name change

---
+++ Adobe Target API project/src/services/getJWTToken.js
@@ -0,0 +1,35 @@
+//console.adobe.io -> Integration -> Details
+
+module.exports = {
+    credentials: {
+        clientId : "bad1cc06ef0349ac978805b6bc8fc341", //Application ID (Client ID)
+        technicalAccountId : "B68766D5602D91140A495FB3@techacct.adobe.com", //Technical account ID
+        orgId : "52A10C3A53DB24890A490D4D@AdobeOrg", // Org ID
+        clientSecret : "4b396f35-8f88-4300-b56d-7189aa304619", //Client Secret
+        metaScopes : "ent_marketing_sdk", // Metascopes
+        privateKey : "-----BEGIN PRIVATE KEY-----\n" +
+		"MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCXowWNKcmBMGIdkMbpEq35ncgr\n" +
+		"EMEsQHZHKBPt8VQdXpZ4egjDC7HMVMRLvVNX7H0m4MfkAk9nmsFKXgkXQfYfUzI7aB/D7oyfn6xm\n" +
+		"AsYz24+FQ58anQ69hGV37BXdzRvstkWEQuheuMshg6JX1iaRg4wGeIBbuOYGjQBynyw5TLOH+CTg\n" +
+		"S2Rpx2C9XRs1sklxDZ4vwj8KgAazZACcxyhGIicWAPGUNx55FJ7mbwTe9DD5M1wgxbL2kwBCTeK/\n" +
+		"5noi2LD4yLHvJBBqk+YEDpojbzVW8eCyrEGVMXbJ7wmv7qFXPsROoZ08ohyMWr7LSYxeSgWuUK5W\n" +
+		"MXY/kJ/y22INAgMBAAECggEAa0ecv9hWhZ3TbJNnBrJ+f2FPophB8qrQg1spbmgUWIr47dd4p3V4\n" +
+		"ntVB40vY0RWxHXZeNUQlJWlpYtHQoLG8DUFxAnKSNq5osu3mrG0+CKZHjcLfp5aCPTaUCxnuBnP8\n" +
+		"U6ZI3F/xwkH/BID6tCYAN2bYBCPwu/O+Wdgb/vv577bnOc3sV5MDUKgCf/7+6l/4bghW1wmGtBgO\n" +
+		"65DhXCFEMy8J7zX67YFk2EE2R+EGsAxl1CUWzn+JrKdVAn4LuQYadIuhkxrGcQlCP63aFDdRnb0M\n" +
+		"PzbuxHbhPurxHz9Cl4KSn6vvHF9aiq2YmCfkKbW4AYPLJVxZXzMKP/EAcg1SSQKBgQDqTRdE1uDr\n" +
+		"lkRGWboVkIVxGbmT9lmKarm+Czt3UDBePaI02COZKf1wZ4IxRYv4F4AAJtVIqep0qaazGY+wlDCt\n" +
+		"MiTziv1PQNXNpRrWD04nW+FzghObFw9COnDNldABMI1WVPqrhwmtEMRNInZoIR2iTOJtvnMq6Hv1\n" +
+		"DwF+Fo+D3wKBgQClrhcdjsfX08q62UxYBrs+5Lw4yH8lP9rDHv/n1D5bwnMsxBOYxh6tzjkXQirj\n" +
+		"yHxijfQjrVPZcpVua5xnjkFLEKAu0i465UFVe723IRR45ClQYO/Rk2kcKmsPBrChY4kI1FCU5YFZ\n" +
+		"3uYhGNCPPPBle+lRaagYrrbggPbXjJp3kwKBgQDdW/oY6p5TVYOpp4n8Z676/N/sn7lW4aAcoYYM\n" +
+		"InGgB8DhBjcTtK0h7lj9bgRgXutquUWWQholIEdDPGoZ3NfkOdTZVz4dC1cz08H37TvcSZ1FwOX/\n" +
+		"B5k96/K4+Mos5Y8SunSda5ZDsOyVJ3MQzACwXs0p+93Y8w5xpzy0wGHUpwKBgCETrJG+G6+/HM5I\n" +
+		"67D3RxK7X8iGXrv1D5u86KJEAV8YZx4c1zy88QsKDOPT1fQ75nlWldM5m2/iLGsrGMKj5+0wmIpA\n" +
+		"c/JWKsECP1H8TLYF9ymQ8NAKegWhglYxo7nUwVr96EiRuU1T7uXUqm6uX+Ai6OXUDjc/Sktv89f8\n" +
+		"vin5AoGAP6dzCfvoKD2LIXDjbm2aie6GorwVbehDD0ILJ0Vi8FD67kSZ+e2yfLzgIT16BPmvgS/H\n" +
+		"keCC9P/7zX/H8sZrp5blSnCatVBzx46IkjYDgzXyPdTBnlLGPXgJigIK03puhnNQ+p3joE8tPE5h\n" +
+		"YWocG/Ts3GSvquLhlAQDHqUtPiE=\n" +
+		"-----END PRIVATE KEY-----\n"
+    }
+}
